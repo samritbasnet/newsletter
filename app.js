14f30675c57cd40ec5fa0e464f919d7817f551fe
samritbasnet <hereiamsamrit7@outlook.com>
2024-09-14 22:08:13 -0400

    finished final changes

---
+++ app.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('newsletter-form');
+    const successMessage = document.getElementById('success-message');
+    const confirmedEmail = document.getElementById('confirmed-email');
+    const dissmissBtn = document.getElementById('dismiss-btn');
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const email = document.getElementById('email').value;
+        confirmedEmail.textContent = email;
+        form.style.display = 'none';
+        successMessage.style.display = 'block';
+
+
+    })
+
+    dissmissBtn.addEventListener('click', function () {
+        successMessage.style.display = 'none';
+        form.style.display = 'block';
+        form.reset()
+    })
+})
